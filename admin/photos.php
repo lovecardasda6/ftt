41bf2989c6281cfd1230f6e5a6b8fd5e3398fca6
lovecardasda6 <32859018+lovecardasda6@users.noreply.github.com>
2019-12-30 17:02:57 +0800

    update

--- admin/photos.php
+++ admin/photos.php
@@ -2,11 +2,22 @@
   require_once __DIR__."/require_files/config.php";
   require_once __DIR__."/require_files/auth.php";
   
+
+  if(isset($_POST['upload']))
+  {
+    $album = mysqli_real_escape_string($con, $_POST['album']);
+    $description = mysqli_real_escape_string($con, $_POST['description']);
+
+    $query = "INSERT INTO albums SET `title` = '".$album."', `description` = '".$album."'";
+    $exec = mysqli_query($con, $query);
+
+  }
+  
 ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Shutter &mdash; Colorlib Website Template</title>
+    <title>Fely's Tours and Travel - Trips for Keeps</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -91,8 +102,24 @@
     </div> <!-- END .site-navbar-wrap -->
 <!----------------------------NAVIGATION--------------------------------------->
 
-    <br>
-    
+<Br>
+
+      <div class="site-section" id="what-we-do-section" style="margin: -75px;">
+        <div class="container">
+
+          <div class="row mb-5">
+            <div class="col-lg-6 section-title">
+              <h2 class="title text-primary">PHOTOS</h2>
+            </div><div class="col-lg-6 section-title" style="text-align:right;">
+              <a href="#">
+                <span class="icon-add" style="font-size:21px;" onclick="$('.upload_album').css('display', 'block ');" >Upload Album</span>
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
 
     <div style="margin:auto; width: 90%; ">
         <main class="main-content" style=" width: 100%; ">
@@ -123,6 +150,7 @@
                             {
                                 while($row2 = mysqli_fetch_assoc($res2))
                                 {
+                                  if($row2['total'] != 0){
                 ?>
                 <!---------------------------------------------------------------------------->
                     <div class="col-6 col-md-6 col-lg-4" data-aos="fade-up">
@@ -138,6 +166,24 @@
                     </div>
                 <!---------------------------------------------------------------------------->
                 <?php
+                                  }
+                                  else{
+                ?>
+                <!---------------------------------------------------------------------------->
+                    <div class="col-6 col-md-6 col-lg-4" data-aos="fade-up">
+                        <a href="single.php?id=<?php echo $row['id']; ?>" class="d-block photo-item">
+                            <img src="./../images/upload.png" alt="Image" class="img-fluid" style='width: 100%;'>
+                            <div class="photo-text-more">
+                            <div class="photo-text-more">
+                            <h3 class="heading"><?php echo $row['title']; ?></h3>
+                            <span class="meta"><?php echo $row2['total']; ?> PHOTOS</span>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                <!---------------------------------------------------------------------------->
+                <?php
+                                  }
                                 }
                             }
                         }
@@ -198,3 +244,49 @@
     
   </body>
 </html>
+
+
+<!----------------------------------------------------------->
+<!-----------------UPLOAD ALBUM START------------->
+<!----------------------------------------------------------->
+<div class="upload_album" 
+style="
+    display: none;
+    position: absolute; 
+    top: 0; 
+    left:0; 
+    background-color: rgba(0,0,0,.5); 
+    width: 100%; 
+    height:100%; 
+    z-index: 2000;
+    align-content:center;
+">
+<Br>
+    <div style="width: 50%; margin:auto;">  
+        <div class="service h-100" style='background-color:white; z-index: 2000;'>
+            <div class="col-lg-6 section-title" style='padding: 25px;'>
+                <span style="font-size:24px; color: rgb(0,123,255); font-weight:bold;">Upload Album</span>
+            </div>
+
+            <form style="padding: 10px 25px; margin-top:8px;" method="POST" autocomplete="off" enctype="multipart/form-data">
+                <label for="avatar">Recommended image size : 450pixel x 450pixel</label>
+                <Br>
+                <input type="file"
+                      id="avatar" name="image"
+                      accept="image/png, image/jpeg">
+                <br><br>
+                <input type="text" require name="album" placeholder="Album Name" style="width: 100%; padding-left: 10px; font-size: 18px; margin-bottom:15px;" /> 
+                <label>Album Description</label>
+                <textarea name="description" style="width: 100%; height: 300px; font-size: 18px; margin-bottom:5px;"> </textarea>
+                <br>
+                <input type="submit" style="border-radius:0px" value="Save" name="upload" class="btn btn-primary"/> 
+                &nbsp; | &nbsp;
+                <button type="button" style="border-radius:0px" onclick="$('.upload_album').css('display', 'none');" class="btn btn-light">Cancel</button>
+            </form>
+
+        </div>
+    </div>
+</div>
+<!----------------------------------------------------------->
+<!-----------------UPLOAD ALBUM END------------->
+<!----------------------------------------------------------->
